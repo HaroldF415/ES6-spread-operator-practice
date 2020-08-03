102c1f9f1b1998e709ac4381086d303e0d4406a1
Harold F <harold.f415@gmail.com>
2020-08-03 18:58:14 -0400

    ES6-spread-operator-practice [complete]

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+
+//CHALLENGE: Make this app work by applying what you've learnt.
+//1. When new text is written into the input, its state should be saved.
+//2. When the add button is pressed, the current data in the input should be
+//added to an array.
+//3. The <ul> should display all the array items as <li>s
 
 function App() {
+  const [input, setInput] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newInput = event.target.value;
+    setInput(newInput);
+  }
+
+  function addItem(event) {
+    setItems(prev => {
+      return [...prev, input];
+    });
+    setInput("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} name="item" value={input} type="text" />
+
+        <button type="submit" onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map(item => (
+            <li>{item}</li>
+          ))}
         </ul>
       </div>
     </div>
